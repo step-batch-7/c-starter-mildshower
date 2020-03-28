838fce9224fb2b0fefdf7cab92b37cd30b3ab705
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-03-28 22:05:53 +0530

    added find_simple_interest function

--- src/basic_functios.c
+++ src/basic_functios.c
@@ -6,6 +6,7 @@ long square(int);
 long cube(int);
 unsigned int gcd(unsigned int, unsigned int);
 unsigned long lcm(unsigned int, unsigned int);
+float find_simple_interest(float, float, float);
 
 unsigned char is_even(int num)
 {
@@ -44,31 +45,45 @@ unsigned long lcm(unsigned num1, unsigned num2)
   return ((unsigned long)num1 * num2) / gcd(num1, num2);
 }
 
+float find_simple_interest(float principal, float interest_rate, float duration)
+{
+  return (principal * interest_rate * duration) / 100;
+}
+
 int main(void)
 {
   int num1, num2, num3, num4;
-  int num5, num6;
+  unsigned num5, num6;
+  float principal, interest_rate, duration;
 
   printf("Enter a number to check whether it is even or not: ");
   scanf("%d",&num1);
   printf("Your Entered Number %d is %s\n\n",num1,is_even(num1)?"Even":"Not Even");
- 
+
   printf("Enter a number to check whether it is odd or not: ");
   scanf("%d",&num2);
   printf("Your Entered Number %d is %s\n\n",num2,is_odd(num2)?"Odd":"Not Odd");
- 
+
   printf("Enter a number to find square: ");
   scanf("%d",&num3);
   printf("Square of entered number %d is %ld\n\n",num3,square(num3));
- 
+
   printf("Enter a number to find cube: ");
   scanf("%d",&num4);
   printf("Cube of entered number %d is %ld\n\n",num4,cube(num4));
 
   printf("Enter two numbers to find GCD and LCM (separate by space): ");
-  scanf("%d %d",&num5,&num6);
-  printf("GCD of %u and %u is: %u\n\n",num5,num6,gcd(num5,num6));
+  scanf("%u%u",&num5,&num6);
+  printf("GCD of %u and %u is: %u\n",num5,num6,gcd(num5,num6));
   printf("LCM of %u and %u is: %lu\n\n",num5,num6,lcm(num5,num6));
+
+  printf("Finding Interest--\nEnter Principal: ");
+  scanf("%f", &principal);
+  printf("Enter Interest Rate: ");
+  scanf("%f", &interest_rate);
+  printf("Enter Duration: ");
+  scanf("%f", &duration);
+  printf("Simple Interest of %.3f at %.3f%%/year for %.3f year(s) is: %.3f\n\n",principal,interest_rate,duration,find_simple_interest(principal,interest_rate,duration));
 
   return 0;
 }
