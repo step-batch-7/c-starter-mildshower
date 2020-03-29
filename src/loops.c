85dbc117d9681fc7c6de714dd9e39c4a769b3e62
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-03-29 20:20:40 +0530

    added print_fibonacci function

--- src/loops.c
+++ src/loops.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 unsigned long find_factorial(unsigned);
+void print_fibonacci(unsigned);
 
 unsigned long find_factorial(unsigned num)
 {
@@ -12,13 +13,28 @@ unsigned long find_factorial(unsigned num)
   return fact;
 }
 
+void print_fibonacci(unsigned elementCount)
+{
+  unsigned long prev = 0, next = 1;
+  for(; elementCount > 0; elementCount--)
+  {
+    printf("%lu\n", prev);
+    next += prev;
+    prev = next - prev;
+  }
+}
+
 int main()
 {
-  unsigned num1;
+  unsigned num1, elementCount;
   
   printf("Enter a number to find factorial: ");
   scanf("%u",&num1);
   printf("Factorial of %u is: %lu\n\n",num1,find_factorial(num1));
+
+  printf("Enter how many fibonacci elements you want: ");
+  scanf("%u",&elementCount);
+  print_fibonacci(elementCount);
 
   return 0;
 }
